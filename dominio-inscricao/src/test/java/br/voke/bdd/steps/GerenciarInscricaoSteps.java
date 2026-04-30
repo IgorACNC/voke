f4ee04eae201a2c44bbe07de9f3c384bb1ef7f2f
Igor Couto <iacncouto@hotmail.com>
2026-04-29 22:43:34 -0300

    feat : correção de pastas e arquivos escritos incorretamente

--- dominio-inscricao/src/test/java/br/voke/bdd/steps/GerenciarInscricaoSteps.java
+++ dominio-inscricao/src/test/java/br/voke/bdd/steps/GerenciarInscricaoSteps.java
@@ -14,7 +14,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
